41bd4ba9a284230b67276bc149ec11d54dd667f1
boringNectarine0 <61633685+boringNectarine0@users.noreply.github.com>
2020-06-12 18:10:55 +1200

    improved steering
    Improve robot's time and accuracy in completing core, wrote method to later implement for challenge to detect red walls

--- robot.cpp
+++ robot.cpp
@@ -1,15 +1,14 @@
-// TODO: REMOVE BEFORE SUBMISSION
 
 #include "robot.hpp"
 #include <vector>
 //check if the current pixel is white
-int doWhiteCheck(int curPix){
-	if(curPix==255){
-		return 1;
-	}
-	else{
-		return 0;
-	}
+int doWhiteCheck(int white){
+	if(white==255){return 1;}
+	else{return 0;}
+}
+int doRedCheck(int redPix){
+	if(redPix==255){return 1;}
+	else{return 0;}
 }
 
 int main(){
@@ -20,7 +19,7 @@ int main(){
 	double vRight = 30.0;
 	takePicture();
 	SavePPMFile("i0.ppm",cameraView);
-	int focusRow = (cameraView.height)/1.5;						//-the chosen row to read the colour of the pixels from
+	
 	int centre = (cameraView.width)/2;							//the middle pixel
 
 	while(1){
@@ -28,20 +27,23 @@ int main(){
 		double shift = 0;											//-distance of middle of white line from centre
 		setMotors(vLeft,vRight);
 		for(int i = 0; i<cameraView.width; i++){
-			int curPix = get_pixel(cameraView, focusRow, i, 3); 						//-looks at the pixel
-			if(doWhiteCheck(curPix)==1) { // if pixel is white, it is part of centre line
+			int focusRow = (cameraView.width)/2;						//-the chosen row to read the colour of the pixels from
+			int whitePix = get_pixel(cameraView, focusRow, i, 3); 						//-looks at the pixel
+			
+			if(doWhiteCheck(whitePix)==1) { // if pixel is white, it is part of centre line
 				shift = centre - i-4; // the centre of view - position of white pixel = distance from centre
 				if (shift == 0) { // if distance from centre is 0
 					vLeft = 30; // set motors to go straight
 					vRight = 30;
 				} else if (shift < 0) { //if robot is left of line
-					vLeft = 35; // set motors to go right
-					vRight = 25;
+					vLeft = 30-(shift/5); // set motors to go right
+					vRight = 30+(shift/5);
 				} else if (shift > 0) { //if robot is right of line
-					vRight = 35; // set motors to go left
-					vLeft = 25;
+					vRight = 30+(shift/5); // set motors to go left
+					vLeft = 30-(shift/5);
 				}
 			}
+		 
 		}
 		std::cout<<" vLeft="<<vLeft<<"  vRight="<<vRight<<std::endl;
 		usleep(10000);
